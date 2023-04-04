c900fa3d3e71e7fc34f80af9723e11870d2bfa7f
mattwvu <53435971+mattwvu@users.noreply.github.com>
2023-04-04 16:26:14 -0400

    Add files via upload

---
+++ tidycensus_workshop.R
@@ -0,0 +1,18 @@
+### Install Packages
+
+install.packages("tidycensus")
+install.packages("tidyverse")
+install.packages("sf")
+install.packages("tigris")
+
+### Load Packages
+
+library(tidycensus)
+library(tidyverse)
+library(sf)
+library(tigris)
+
+### Set your working directory
+
+
+
